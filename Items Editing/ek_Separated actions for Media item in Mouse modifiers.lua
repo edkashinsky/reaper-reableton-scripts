9cd5dfd0dc49ea3a8c51fd11923c1a47cc0a7be9
Ed Kashinsky <ed.kashinsky@gmail.com>
2024-07-14 13:13:41 +0200

    bug fixes

--- Items Editing/ek_Separated actions for Media item in Mouse modifiers.lua
+++ Items Editing/ek_Separated actions for Media item in Mouse modifiers.lua
@@ -1,5 +1,5 @@
 -- @description ek_Separated actions for Media item in Mouse modifiers
--- @version 1.1.2
+-- @version 1.1.3
 -- @author Ed Kashinsky
 -- @readme_skip
 -- @about
@@ -9,8 +9,7 @@
 --		- If option enabled, header label is positioned above the item and "header part" calculates as 1/4 of the upper part of the item
 --      - If option disabled, header label is positioned on item and header part calculates as header label height
 -- @changelog
---   - Script works in 2 workflows depends on option "Draw labels above the item when media item height is more than": If option disabled, header label is positioned on item and header part calculates as header label height. If option enabled, header label is positioned above the item and "header part" calculates as 1/4 of the upper part of the item
---   - Fixed bug when script doesn't work with lanes
+--   Script monitors media item label font size and UI scale as well
 
 if not reaper.APIExists("JS_ReaScriptAPI_Version") then
 	local answer = reaper.MB("You have to install JS_ReaScriptAPI for this script to work. Would you like to open the relative web page in your browser?", "JS_ReaScriptAPI not installed", 4 )
@@ -36,6 +35,71 @@ if not loaded then
 	return
 end
 
+local function GetMediaItemFontData()
+    -- GET MEDIA ITEM LABEL FONT FROM COLORTHEME_FILE OR REAPER.INI
+    local inipath = reaper.get_ini_file()
+    local mi_font
+    local _, lasttheme = reaper.BR_Win32_GetPrivateProfileString("reaper", "lastthemefn5", "Error", inipath)
+    if lasttheme == "*unsaved*" then
+        _, mi_font = reaper.BR_Win32_GetPrivateProfileString("reaper", "mi_font", "Error", inipath)
+    else
+        local theme = reaper.GetLastColorThemeFile()
+        _, mi_font = reaper.BR_Win32_GetPrivateProfileString("reaper", "mi_font", "Error", theme)
+        if mi_font == "Error" then
+            local ext = theme:find("(.-)%Zip")
+            if not ext then
+                theme = theme .. "Zip"
+            end
+            local zip, _ = reaper.JS_Zip_Open(theme, 'r', 6)
+            local _, ent_str = reaper.JS_Zip_ListAllEntries(zip)
+            local file_name
+            for name in ent_str:gmatch("[^\0]+") do
+                local file = name:match("(.-)%.ReaperTheme$")
+                if file then
+                    file_name = name
+                    break
+                end
+            end
+            reaper.JS_Zip_Entry_OpenByName(zip, file_name)
+            local _, contents = reaper.JS_Zip_Entry_ExtractToMemory(zip)
+            mi_font = string.match(tostring(contents), "mi_font=(%x*)")
+            reaper.JS_Zip_Entry_Close(zip)
+            reaper.JS_Zip_Close(theme)
+        end
+    end
+
+    mi_font = mi_font:gsub(('[A-F0-9]'):rep(2), function(byte)
+        return string.char(tonumber(byte, 16))
+    end)
+
+    return ('iiiiibbbbbbbbc32'):unpack(mi_font)
+end
+
+local function GetLabelHeight()
+    local scale = reaper.SNM_GetDoubleConfigVar("uiscale", 1)
+    local font_size, width, escapement, orientation, weight, italic, underline, strike_out,
+    charset, out_precision, clip_precision, quality, pitch_and_family, facename = GetMediaItemFontData()
+
+    -- GFX TEXT MEASUREMENT --
+    local flags = ''
+    if weight > 649 then flags = 'b' end
+    if italic == -1 then flags = flags .. 'i' end
+    if underline == -1 then flags = flags .. 'u' end
+
+    local function fontflags(str)
+        local v = 0
+        for a = 1, str:len() do v = v * 256 + string.byte(str, a) end
+        return v
+    end
+
+    -- Trying to draw debug text and measure how much size it takes
+    -- + 1 - it's compensation for padding
+    gfx.setfont(1, facename, font_size + 1, fontflags(flags))
+    local _, height = gfx.measurestr("QWj_09")
+
+    return height * scale
+end
+
 local _, _, _, cmdID, _, _, _ = reaper.get_action_context()
 local hKey_id = "separated_actions_cmd_h:"
 local tKey_id = "separated_actions_cmd_t:"
@@ -47,16 +111,8 @@ local MainHwnd = reaper.GetMainHwnd()
 local ArrangeHwnd = reaper.JS_Window_FindChildByID(MainHwnd, 0x3E8)
 local x, y = reaper.GetMousePosition()
 local _, ry = reaper.JS_Window_ScreenToClient(ArrangeHwnd, x, y)
-
-local headerHeight = 16
-local headerShowingLimit = 40
 local item, _ = reaper.GetItemFromPoint(x, y, true)
 local isSettingsNeeded = not item and reaper.JS_Window_GetFocus() ~= ArrangeHwnd
-
-if gfx.ext_retina == 1 then
-	headerHeight = headerHeight * 2
-	headerShowingLimit = 70
-end
 
 if (not header_cmd_id and not item_cmd_id) or isSettingsNeeded then
 	local isAnyActionSet = false
@@ -96,7 +152,14 @@ local track_y = reaper.GetMediaTrackInfo_Value(track, "I_TCPY") + reaper.GetMedi
 if GetItemHeaderHeight(item) == 0 then
 	-- "Draw labels above the item when media item height is more than" is disabled
 	-- Click on header
-	track_y = track_y + headerHeight
+    local height_key = "separate_actions_label_height"
+    local height = EK_GetExtState(height_key)
+    if not height then
+        height = GetLabelHeight()
+        EK_SetExtState(height_key, height, false, true)
+    end
+
+	track_y = track_y + height
 else
 	-- "Draw labels above the item when media item height is more than" is enabled
 	-- Click on 1/4 up part of item
