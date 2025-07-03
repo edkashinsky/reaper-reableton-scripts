5b38c0e1b40f2e54d0bc3d44018daadd8983937d
Ed Kashinsky <ed.kashinsky@gmail.com>
2025-07-03 12:56:36 +0200

    added pitch tool script

--- Items Editing/ek_Pitch Tool.lua
+++ Items Editing/ek_Pitch Tool.lua
@@ -1,19 +1,34 @@
 -- @author Ed Kashinsky
 -- @description ek_Pitch Tool
--- @version 1.0.0
--- @metapackage
+-- @version 2.0.0
 -- @about
 --    Pitch tool brings Ableton workflow for pitch manipulations of audio clips.
---       - Change pitch mode for selected items menu
---       - Toggle preserve pitch for selected items
---       - Increase/Decrease pitch or rate for selected items
---       - Switch to prev/next pitch mode for selected items
---       - Clear pitch or rate for selected items
+--		- Ableton-style pitch shifting for intuitive and musical pitch control
+--		- Detailed pitch-stretching adjustments with a user-friendly interface
+--		- Multi-mode control for adjusting the pitch of an unlimited number of items
+--		- Flexible window docking, plus the option to display the tool contextually above selected items
+--		- Theme-adaptive interface that matches your REAPER theme for seamless visual integration
+-- @links
+--   Documentation https://github.com/edkashinsky/reaper-reableton-scripts/wiki/Pitch-Tool
+--   Buy Licence https://ekscripts.gumroad.com/l/pitch-tool
 -- @provides
---   [main=main] ek_Pitch Tool - Change pitch mode for selected items menu.lua
---   [main=main] ek_Pitch Tool - Clear pitch or rate for selected items.lua
---   [main=main] ek_Pitch Tool - Decrease pitch or rate for selected items.lua
---   [main=main] ek_Pitch Tool - Increase pitch or rate for selected items.lua
---   [main=main] ek_Pitch Tool - Switch to next pitch mode for selected items.lua
---   [main=main] ek_Pitch Tool - Switch to prev pitch mode for selected items.lua
---   [main=main] ek_Pitch Tool - Toggle preserve pitch for selected items.lua
+--   ../Core/data/pitch-tool_*.dat
+--   [main=main] ek_Pitch Tool - Tooltip (background).lua
+
+local function CoreLibraryLoad(lib)
+	local sep = package.config:sub(1,1)
+	local root_path = debug.getinfo(1, 'S').source:sub(2, -5):match("(.*" .. sep .. ")")
+	local version = string.match(_VERSION, "%d+%.?%d*")
+	local dat_path = root_path .. ".." .. sep .. "Core" .. sep .. "data" .. sep .. lib .. "_" .. version .. ".dat"
+	local file = io.open(dat_path, 'r')
+
+	if file then file:close() dofile(dat_path) return true else return false end
+end
+
+if not CoreLibraryLoad("core") or not CoreLibraryLoad("pitch-tool") then
+	reaper.MB('Core functions is missing. Please install "ek_Core functions" it via ReaPack (Action: Browse packages)', '', 0)
+	reaper.ReaPack_BrowsePackages("ek_Core functions")
+	return
+end
+
+PT_ShowGui()
