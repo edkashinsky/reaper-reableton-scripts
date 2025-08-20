a5d3445bd144998df4b959b68bd0b76eee11160c
Ed Kashinsky <ed.kashinsky@gmail.com>
2025-08-20 13:32:20 +0200

    updated rrm-filler

--- Regions/ek_Region Render Matrix Filler.lua
+++ Regions/ek_Region Render Matrix Filler.lua
@@ -1,6 +1,6 @@
 --[[
 @description ek_Region Render Matrix Filler
-@version 1.0.10
+@version 1.0.11
 @author Ed Kashinsky
 @about
   Region Render Matrix Filler significantly speeds up the process of filling the Render Matrix in REAPER, especially in projects with a large number of regions. It’s particularly useful for tasks like layer-based sound rendering, gameplay VO synced to video, voiceover exports, and other scenarios where batch rendering is needed.
@@ -11,20 +11,7 @@
      * Manual override of track assignment for individual regions
      * Ability to rename regions or tracks
 @changelog
-	* New Length field for regions – Displays and allows editing of region length.
-	* Improved script performance – Runs faster and more efficiently.
-	* Bug fix: Overridden track not always highlighted in the table.
-	* Bug fix: Track hierarchy for override not always displayed correctly.
-	* Hierarch Offset field redesigned – Now supports drag-and-drop adjustment. No need to enter -1 for Master; simply drag to set the desired value.
-	* ImGui 0.10 Support
-	* "Close window after action" option moved – This setting is now located in the About window.
-	* Improved keyboard navigation – Use Tab / Shift+Tab or the Left/Right arrow keys to move focus between input fields. Use the Up/Down arrow keys to perform actions.
-	* Dynamic font updates in the About window – Font changes now apply instantly without reopening the window.
-	* New Help button in About – Provides quick access to documentation and the related forum thread.
-	* Better link display – Links are now easier to read and click.
-	* Improved tooltips – More readable and better positioned.
-	* Enhanced input field behavior – More consistent and user-friendly across different types of fields.
-	* Library dependency check added – The application now verifies that all required libraries are present before running.
+	* Added "Repeat override" button for region which apply last track override for each region
 @links
 	Documentation https://github.com/edkashinsky/reaper-reableton-scripts/wiki/Render-Region-Matrix-Filler
 	Forum Thread https://forum.cockos.com/showthread.php?t=300927
