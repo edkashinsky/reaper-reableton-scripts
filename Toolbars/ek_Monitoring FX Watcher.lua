aaa7318cd04b9a01649c6fd777983279767284c3
Ed Kashinsky <ed.kashinsky@gmail.com>
2026-01-05 01:39:20 +0100

    small fixes

--- Toolbars/ek_Monitoring FX Watcher.lua
+++ Toolbars/ek_Monitoring FX Watcher.lua
@@ -1,6 +1,6 @@
 -- @author Ed Kashinsky
 -- @description ek_Monitoring FX Watcher
--- @version 1.0.1
+-- @version 1.0.2
 -- @about
 --   This script monitors a certain fx slot in the monitoring chain and switches the bypass on it in realtime.
 -- @changelog
